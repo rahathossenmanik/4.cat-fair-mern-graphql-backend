0b0e5b842d670b92da5ac4fe49ebdc490d963253
Manik Hosen <manik.hosen@bizzntek.com>
2023-03-16 17:19:33 +0600

    feat: mutation added

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,10 @@
+const { mutations } = require('./mutations');
 const { queries, author, book, date } = require('./queries');
 
 exports.resolvers = {
   Date: date,
   Query: queries,
+  Mutation: mutations,
   Author: author,
   Book: book
 };
