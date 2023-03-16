5ecb2b195a8f38ba82252f28185a786a02ba3ef3
Manik Hosen <manik.hosen@bizzntek.com>
2023-03-16 10:55:32 +0600

    ci: graphql only

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require('dotenv').config({ path: '.env' });
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const mongoose = require('mongoose');
-const authors = require('./routes/authors');
-const books = require('./routes/books');
 const { typeDefs, resolvers } = require('./graphql/schema');
 
 const port = process.env.PORT;
@@ -26,17 +24,8 @@ const startAppolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
 
-  app.use(express.json());
-  app.use('/api/authors', authors);
-  app.use('/api/books', books);
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
-
   app.listen({ port }, () => {
-    console.log(`REST API is Running on: http://localhost:${port}`);
-    console.log(`GraphQL is Running on: http://localhost:${port}${server.graphqlPath}`);
+    console.log(`Server is Running on: http://localhost:${port}${server.graphqlPath}`);
   });
 };
 
